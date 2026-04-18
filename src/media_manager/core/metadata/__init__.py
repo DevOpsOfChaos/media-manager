b6e8b204b1e570045c24ac15220af10d1ba490bd
Manu <mries444@gmail.com>
2026-04-18 14:26:23 +0200

    feat: add metadata inspection exports

--- src/media_manager/core/metadata/__init__.py
+++ src/media_manager/core/metadata/__init__.py
@@ -1,1 +1,11 @@
-"""Metadata module scaffold for the rebuilt core."""
+"""Metadata module for the rebuilt core."""
+
+from .inspect import extract_date_candidates, inspect_media_file
+from .models import DateCandidate, FileInspection
+
+__all__ = [
+    "DateCandidate",
+    "FileInspection",
+    "extract_date_candidates",
+    "inspect_media_file",
+]
