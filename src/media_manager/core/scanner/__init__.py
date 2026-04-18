4d61c5ab28105a1c738d049a6c50a3e2f5d6550e
Manu <mries444@gmail.com>
2026-04-18 14:08:59 +0200

    feat: add scanner core exports

--- src/media_manager/core/scanner/__init__.py
+++ src/media_manager/core/scanner/__init__.py
@@ -1,1 +1,11 @@
-"""Scanner module scaffold for the rebuilt core."""
+"""Scanner module for the rebuilt core."""
+
+from .discovery import scan_media_sources
+from .models import ScanOptions, ScanSummary, ScannedFile
+
+__all__ = [
+    "ScanOptions",
+    "ScannedFile",
+    "ScanSummary",
+    "scan_media_sources",
+]
